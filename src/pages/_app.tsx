c7109dc82bd4c1846db40b049bd232759f0b345b
wb1110 <williamjaredbuchanan@gmail.com>
2023-03-03 11:42:12 -0600

    front egg setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
-import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { withFronteggApp } from "@frontegg/nextjs";
 
-export default function App({ Component, pageProps }: AppProps) {
+function CustomApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
+
+export default withFronteggApp(CustomApp, {
+  hostedLoginBox: true,
+  authOptions: {
+    // keepSessionAlive: true // Uncomment this in order to maintain the session alive
+  },
+});
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,39 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "next/font/google";
+import { GetServerSideProps } from "next";
+import { withSSRSession, useAuth } from "@frontegg/nextjs";
 
-export default function Home() {
+export default function MyPage({ products }) {
+  const { user } = useAuth();
+
+  //baseUrl should be your FRONTEGG_APP_URL from .env.local
+  const baseUrl = "http://localhost:3000";
+
+  const logout = () => {
+    window.location.href = `${baseUrl}/account/logout`;
+  };
+
   return (
     <div>
-      <p>test</p>
+      <h1>My Page</h1>
+      <div>
+        <span>Logged in as: {user?.name}</span>
+      </div>
+      <div>
+        <button onClick={logout}>Log out</button>
+      </div>
     </div>
   );
 }
+
+// In the `getServerSideProps` method you can get data from an external service to pull relevant data for a logged in user.
+// we used the prop `products`. See the commented code for an example.
+
+export const getServerSideProps: GetServerSideProps = withSSRSession(
+  async (context, session) => {
+    //     const { data } = await fetch('{external}/product', {
+    //      headers: {
+    //        Authorization: 'bearer ' + session.accessToken,
+    //      },
+    //    });
+    return { props: {} };
+  }
+);

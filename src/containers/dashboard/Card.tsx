ff025745eb4d1938ba2ea485648b412db6fd9492
wb1110 <williamjaredbuchanan@gmail.com>
2023-03-06 15:48:05 -0600

    dashboard filler working

---
+++ src/containers/dashboard/Card.tsx
@@ -0,0 +1,19 @@
+import Typography from "@mui/material/Typography";
+import * as React from "react";
+import Title from "./Title";
+
+interface CardProps {
+  title: string;
+  value: string;
+}
+
+export default function Card({ title, value }: CardProps) {
+  return (
+    <React.Fragment>
+      <Title>{title}</Title>
+      <Typography component="p" variant="h4">
+        {value}
+      </Typography>
+    </React.Fragment>
+  );
+}

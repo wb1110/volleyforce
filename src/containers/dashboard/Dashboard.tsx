3d06dc5bcb55d81b080408633e3c2978a475c25c
wb1110 <williamjaredbuchanan@gmail.com>
2023-03-06 15:11:04 -0600

    refactor

--- src/containers/dashboard/Dashboard.tsx
+++ src/containers/dashboard/Dashboard.tsx
@@ -11,12 +11,10 @@ import MuiDrawer from "@mui/material/Drawer";
 import IconButton from "@mui/material/IconButton";
 import Link from "@mui/material/Link";
 import List from "@mui/material/List";
-import { createTheme, styled, ThemeProvider } from "@mui/material/styles";
+import { styled } from "@mui/material/styles";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import * as React from "react";
-import Services from "../services/Services";
-import DashboardSelected from "./DashboardSelected";
 import { MainListItems, secondaryListItems } from "./listItems";
 
 function Copyright(props: any) {
@@ -87,17 +85,6 @@ const Drawer = styled(MuiDrawer, {
   },
 }));
 
-const mdTheme = createTheme({
-  palette: {
-    primary: {
-      main: "#363740",
-    },
-    secondary: {
-      main: "#FFFFFF",
-    },
-  },
-});
-
 function DashboardContent() {
   const { user } = useAuth();
   const [currentLink, setCurrentLink] = React.useState("dashboard");
@@ -122,96 +109,94 @@ function DashboardContent() {
   };
 
   return (
-    <ThemeProvider theme={mdTheme}>
-      <Box sx={{ display: "flex" }}>
-        <CssBaseline />
-        <AppBar position="absolute" open={open}>
-          <Toolbar
-            sx={{
-              pr: "24px", // keep right padding when drawer closed
-            }}
-          >
-            <IconButton
-              edge="start"
-              color="inherit"
-              aria-label="open drawer"
-              onClick={toggleDrawer}
-              sx={{
-                marginRight: "36px",
-                ...(open && { display: "none" }),
-              }}
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography
-              component="h1"
-              variant="h6"
-              color="inherit"
-              noWrap
-              sx={{ flexGrow: 1 }}
-            >
-              {dynamicTitle(currentLink)}
-            </Typography>
-            <div
-              style={{
-                width: "20%",
-                justifyContent: "space-between",
-                display: "flex",
-                alignItems: "center",
-              }}
-            >
-              <span>{user?.name}</span>
-              <Button onClick={logout} variant="contained" color="secondary">
-                Log out
-              </Button>
-              <IconButton color="inherit"></IconButton>
-            </div>
-          </Toolbar>
-        </AppBar>
-        <Drawer variant="permanent" open={open}>
-          <Toolbar
-            sx={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "flex-end",
-              px: [1],
-            }}
-          >
-            <IconButton onClick={toggleDrawer}>
-              <ChevronLeftIcon />
-            </IconButton>
-          </Toolbar>
-          <Divider />
-          <List component="nav">
-            <MainListItems
-              currentLink={currentLink}
-              setCurrentLink={setCurrentLink}
-            />
-            <Divider sx={{ my: 1 }} />
-            {secondaryListItems}
-          </List>
-        </Drawer>
-        <Box
-          component="main"
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <AppBar position="absolute" open={open}>
+        <Toolbar
           sx={{
-            backgroundColor: (theme) =>
-              theme.palette.mode === "light"
-                ? theme.palette.grey[100]
-                : theme.palette.grey[900],
-            flexGrow: 1,
-            height: "100vh",
-            overflow: "auto",
+            pr: "24px", // keep right padding when drawer closed
           }}
         >
-          <Toolbar />
-          <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
-            {/* {currentLink === 'dashboard' && <DashboardSelected />}
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="open drawer"
+            onClick={toggleDrawer}
+            sx={{
+              marginRight: "36px",
+              ...(open && { display: "none" }),
+            }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            component="h1"
+            variant="h6"
+            color="inherit"
+            noWrap
+            sx={{ flexGrow: 1 }}
+          >
+            {dynamicTitle(currentLink)}
+          </Typography>
+          <div
+            style={{
+              width: "20%",
+              justifyContent: "space-between",
+              display: "flex",
+              alignItems: "center",
+            }}
+          >
+            <span>{user?.name}</span>
+            <Button onClick={logout} variant="contained" color="secondary">
+              Log out
+            </Button>
+            <IconButton color="inherit"></IconButton>
+          </div>
+        </Toolbar>
+      </AppBar>
+      <Drawer variant="permanent" open={open}>
+        <Toolbar
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "flex-end",
+            px: [1],
+          }}
+        >
+          <IconButton onClick={toggleDrawer}>
+            <ChevronLeftIcon />
+          </IconButton>
+        </Toolbar>
+        <Divider />
+        <List component="nav">
+          <MainListItems
+            currentLink={currentLink}
+            setCurrentLink={setCurrentLink}
+          />
+          <Divider sx={{ my: 1 }} />
+          {secondaryListItems}
+        </List>
+      </Drawer>
+      <Box
+        component="main"
+        sx={{
+          backgroundColor: (theme) =>
+            theme.palette.mode === "light"
+              ? theme.palette.grey[100]
+              : theme.palette.grey[900],
+          flexGrow: 1,
+          height: "100vh",
+          overflow: "auto",
+        }}
+      >
+        <Toolbar />
+        <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+          {/* {currentLink === 'dashboard' && <DashboardSelected />}
             {currentLink === 'services' && <Services />} */}
-            <Copyright sx={{ pt: 4 }} />
-          </Container>
-        </Box>
+          <Copyright sx={{ pt: 4 }} />
+        </Container>
       </Box>
-    </ThemeProvider>
+    </Box>
   );
 }
 

41400ae5aebf5f533119a762f67fc2a8c8d048e1
wb1110 <williamjaredbuchanan@gmail.com>
2023-03-06 14:53:00 -0600

    e2e setup working

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,21 +1,29 @@
 // Import Playwright's test runner
 import { test, expect } from "@playwright/test";
 
+// Logs in test user
+test.beforeEach(async ({ page }) => {
+  await page.goto("/");
+  await page.waitForURL(
+    "https://app-yec57rbn7c0e.frontegg.com/oauth/account/login"
+  );
+  await page.fill('[name="email"]', "strengthcoachwb@gmail.com");
+  await page.fill('[name="password"]', "Password1!");
+  await page.click('[type="submit"]');
+});
+
 // Define a test suite
 test.describe("My Page", () => {
   // Define a test case
   test("should log in the user", async ({ page }) => {
-    await page.goto("/");
-    // You are signed in!
-    //   await page.goto("/");
-    await page.waitForURL(
-      "https://app-yec57rbn7c0e.frontegg.com/oauth/account/login"
-    );
-    await page.fill('[name="email"]', "strengthcoachwb@gmail.com");
-    await page.fill('[name="password"]', "Password1!");
-    await page.click('[type="submit"]');
-
     // Wait for the user name to be visible
     await page.waitForSelector("text=Logged in as:");
   });
+});
+
+// Goes to home page and logs out
+test.afterAll(async ({ page }) => {
+  await page.goto("/");
+  await page.click('button:text("Log out")');
+  await expect(page).toHaveURL("http://localhost:3000/account/logout");
 });

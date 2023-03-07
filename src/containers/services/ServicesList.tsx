53be6181f3fc91c728fd3ef8b7a95e0ac0598634
wb1110 <williamjaredbuchanan@gmail.com>
2023-03-07 13:39:38 -0600

    service data

--- src/containers/services/ServicesList.tsx
+++ src/containers/services/ServicesList.tsx
@@ -10,87 +10,43 @@ import Title from "./Title";
 // Generate Order Data
 function createData(
   id: number,
-  date: string,
-  name: string,
-  shipTo: string,
-  paymentMethod: string,
+  serviceName: string,
+  serviceType: string,
   amount: number
 ) {
-  return { id, date, name, shipTo, paymentMethod, amount };
+  return { id, serviceName, serviceType, amount };
 }
 
 const rows = [
-  createData(
-    0,
-    "16 Mar, 2019",
-    "Elvis Presley",
-    "Tupelo, MS",
-    "VISA ⠀•••• 3719",
-    312.44
-  ),
-  createData(
-    1,
-    "16 Mar, 2019",
-    "Paul McCartney",
-    "London, UK",
-    "VISA ⠀•••• 2574",
-    866.99
-  ),
-  createData(
-    2,
-    "16 Mar, 2019",
-    "Tom Scholz",
-    "Boston, MA",
-    "MC ⠀•••• 1253",
-    100.81
-  ),
-  createData(
-    3,
-    "16 Mar, 2019",
-    "Michael Jackson",
-    "Gary, IN",
-    "AMEX ⠀•••• 2000",
-    654.39
-  ),
-  createData(
-    4,
-    "15 Mar, 2019",
-    "Bruce Springsteen",
-    "Long Branch, NJ",
-    "VISA ⠀•••• 5919",
-    212.79
-  ),
+  createData(0, "Club Dues", "Club", 500.0),
+  createData(1, "Skills Clinic", "Clinics/Camps", 50.0),
+  createData(2, "Summer Camp", "Clinics/Camps", 200.0),
+  createData(3, "Court Rental", "Rental", 50.0),
+  createData(4, "Spring League 1", "League", 500.0),
 ];
-
-function preventDefault(event: React.MouseEvent) {
-  event.preventDefault();
-}
 
 export default function ServicesList() {
   return (
     <React.Fragment>
-      <Title>Recent Transactions</Title>
+      <Title>Services Offered</Title>
       <Table size="small">
         <TableHead>
           <TableRow>
-            <TableCell>Date</TableCell>
-            <TableCell>Name</TableCell>
+            <TableCell>Service Name</TableCell>
+            <TableCell>Service Type</TableCell>
             <TableCell align="right">Amount</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {rows.map((row) => (
             <TableRow key={row.id}>
-              <TableCell>{row.date}</TableCell>
-              <TableCell>{row.name}</TableCell>
+              <TableCell>{row.serviceName}</TableCell>
+              <TableCell>{row.serviceType}</TableCell>
               <TableCell align="right">{`$${row.amount}`}</TableCell>
             </TableRow>
           ))}
         </TableBody>
       </Table>
-      <Link color="primary" href="#" onClick={preventDefault} sx={{ mt: 3 }}>
-        See more transactions
-      </Link>
     </React.Fragment>
   );
 }
